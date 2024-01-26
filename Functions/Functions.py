085db28499ee8e4922c49eb4921d05b49dc379a6
Derpentolongist <62804814+Derpentolongist@users.noreply.github.com>
2024-01-26 15:57:32 +0800

    New Addition Part 3

---
+++ Functions/Functions.py
@@ -0,0 +1,7 @@
+def Hello_Hi(name, age):
+    print("Hello, "+ name + ", you are "+age+ " years old!")
+    
+
+
+Hello_Hi("Mike", "34")
+Hello_Hi("Steve", "55")
---
+++ IfStatement/DesicionStatements.py
@@ -0,0 +1,7 @@
+is_male = True
+
+if is_male:
+    print("You are indeed a male!")
+
+else:
+    print("You not male ;<")
