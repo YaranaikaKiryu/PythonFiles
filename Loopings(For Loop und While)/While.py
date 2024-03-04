b23fa0a9e2eac17ef14bbdab729bd46ec0c30726
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2024-03-05 00:07:03 +0800

    loop

--- Loopings(For Loop und While)/While.py
+++ Loopings(For Loop und While)/While.py
@@ -51,7 +51,8 @@ print(weekSalary) """
 
 
 
-""" sum = 0 
+""" 
+sum = 0 
 total = 0
 number = int(input("ENter a Number and Enter Zero if you're done >> "))
 
@@ -66,3 +67,147 @@ if total > 0:
 else:
     print("No numbers were entered.") """
 
+
+# count = 0
+# while count < 5:
+#     count = count + 1
+#     print("I am Loop Number",count)
+# print("End Of Loop")
+    
+    
+
+# attempts = 0 
+# while True:
+ 
+#  name = input("Enter Name >> ")
+ 
+#  if name == 'Mark':
+#      print("Hello Mark!")
+#      break
+#  else:
+#      print("HOLY SHIT YOU ARE NOT MARK!")
+#      attempts += 1
+#      if attempts == 3:
+#          uppername = name.upper()
+#          print("FUCK OFF "+uppername+" YOU'RE NOT MARK!")
+#          break
+     
+
+
+# Iteration = int(input("Enter Number of Iteration >> "))
+
+# counter = 1
+# avg_count = 0
+# sum = 0
+
+# for i in range(0,Iteration):
+#     value = float(input("Enter Value " + str(counter) + " >> "))
+#     counter +=1
+#     sum+= value
+#     avg_count += 1
+    
+# if avg_count > 0:
+#     print("SUM >> ",sum)
+#     average = sum/avg_count
+#     print("AVG >> ",average)
+
+# else:
+#     print("No Numbers were Inputted")
+    
+    
+#MULTIPLICATION TABLE
+# number = int(input("Enter a number >> "))
+# endVal = int(input("Enter a End Value >> "))
+
+# for i in range(1,endVal):
+#     product = number * i
+#     print(product)
+    
+    
+# numbers = [12,75,150,180,145,525,50]
+
+# for items in numbers:
+#     if items > 500:
+#         break
+#     elif items > 150:
+#         continue
+    
+#     elif (items % 5 == 0):
+#         print(items)
+
+
+# counter = 0 
+# number = 75869
+
+# while number != 0:
+#     number = number // 10
+#     counter += 1
+# print(counter)
+
+
+
+# list1 = [10,20,30,40,50]
+
+# list1 = reversed(list1)
+
+# for items in list1:
+#     print(items)
+
+
+
+# for items in range(-10,0,1):
+#     print(items)
+
+
+# start = 25
+# end = 50
+
+# for num in range(start,end+1):
+#     if num > 1:
+#         for i in range(2,num):
+#             if (num % i) == 0:
+#                 break
+            
+#         else:
+#             print(num)
+
+
+# #Reverses a Word
+# str = input("Word >> ")
+# for i in range(len(str)-1,-1,-1):
+#     print(str[i], end="")
+# print("\n")
+
+# counter = 0
+# number = 43521
+
+# while number != 0:
+#     rem = number % 10
+#     counter = counter * 10 + rem
+#     number=number //10
+    
+# print(counter)
+    
+   
+# n = int(input("Enter a Range >> "))
+# while n != 0:
+#     print(n, end=" ")
+#     n = n-1
+    
+    
+
+sum_even = 0
+sum_odd = 0
+
+for i in range(1011):
+    
+    if i%2 == 0:
+        sum_even+=1
+        
+    else:
+        sum_odd+=1
+        
+        
+print("The sum of all even numbers is", sum_even)
+print("The sum of all odd numbers is", sum_odd)
+        
