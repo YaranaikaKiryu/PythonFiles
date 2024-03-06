e15a5a073437e7a706e50eaf1000b7c897fb7ff9
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2024-03-07 00:24:58 +0800

    Part 5k

--- Zed_Designation_CC3_Activities/Activity#5.py
+++ Zed_Designation_CC3_Activities/Activity#5.py
@@ -1,41 +1,57 @@
+
+
 Name = input("Enter Ur Name >> ")
-
-Math = int(input("Enter Grade In Math >> "))
-
-Science = int(input("Enter Grade In Science >> "))
-
-English = int(input("Enter Grade In English >> "))
-
-
-Sum = Math + Science + English
-Average = Sum/3
-
-print("===============")
-print("Name >> ",Name)
-
-print("Math >> ",Math)
-print("Science >> ",Science)
-print("English >> ",English)
-print("===============")
-print("Average >> ",round(Average))
-
 try:
-    if Average > 74:
-        print("you Passed the semester")
-    else:
-        print("You Failed the semester")
-        raise Exception
-        
-except:
-      if Math <= 74:
-        print("You Failed Math")
+  Math = int(input("Enter Grade In Math >> "))
+  Science = int(input("Enter Grade In Science >> "))
+  English = int(input("Enter Grade In English >> "))
 
-      if Science <= 74:
-        print("You Failed Science")
-        
-      if English <= 74:
-        print("you Failed English")
+    
+except ValueError:
+    print("Please Enter A Number")
 
-finally:
-    print("===============")
-    print("Program Will Run No Matter What")
+else:
+ 
+
+  Sum = Math + Science + English
+  Average = Sum/3
+
+
+
+
+  print("===============")
+  print("Name >> ",Name)
+
+  print("Math >> ",Math)
+  print("Science >> ",Science)
+  print("English >> ",English)
+  print("===============")
+  print("Average >> ",round(Average))
+
+
+  
+  if Average < 75:
+    print("you fail")
+  else:
+    print("you pass")
+    
+
+
+  try:
+    if Math < 75:
+      raise Exception
+  except Exception:
+    print("You Failed Math")
+    
+  try:
+    if Science < 75:
+      raise Exception
+  except Exception:
+    print("You Failed Science")
+    
+  try:
+    if English < 75:
+      raise Exception
+  except Exception:
+    print("You Failed English")
+    
