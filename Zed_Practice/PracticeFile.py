e0043e2b474f0306c2efec62ddf4e5609966b2c5
Derpentolongist <62804814+Derpentolongist@users.noreply.github.com>
2024-03-11 22:49:32 +0800

    d

---
+++ Zed_Practice/PracticeFile.py
@@ -0,0 +1,39 @@
+""" NumberOFValues = int(input("Enter Number of Values >> "))
+
+
+values = []
+for i in range(NumberOFValues):
+    Value = int(input(f"Enter Value {i+1} >> "))
+    values.append(Value)
+
+print(list(reversed(values))) """
+
+
+""" rows = int(input("Enter the number of rows: "))
+multiplier = int(input("Enter the multiplier: "))
+print("==========")
+for x in range(rows):
+    for y in range(x+1):
+        print(multiplier*y, end =" ")
+    print("\r")
+
+ """
+
+""" rows = int(input("Enter the number of rows: "))
+column = int(input("Enter the number of columns: "))
+
+for x in range(1,rows+1):
+    for y in range(1,column+1):
+        print(x*y, end="\t ")
+    print() """
+    
+""" 
+rows = int(input("Enter the number of rows: "))
+multiplier = int(input("Enter the multiplier: "))
+
+for x in range(rows):
+    for y in range(x+1):
+        print(multiplier*y,end="-")
+    print("\r") """
+
+
