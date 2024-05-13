1c6e2ee3ffbaaa13adccdf1448f39520b6b8444b
YaranaikaKiryu <146524104+YaranaikaKiryu@users.noreply.github.com>
2024-05-13 13:37:20 +0800

    rtyrt

--- FinalTermPractice/PracticeFile1.py
+++ FinalTermPractice/PracticeFile1.py
@@ -1,54 +1,71 @@
-class Playlist:
-    def __init__(self):
-        self.songs = []
-       
-    def add_song(self, song_name, song_artist):
-        song = {"name": song_name, "artist": song_artist}
-        self.songs.append(song)
-        print(f"{song_name} has been added to the playlist.")
-    
-    def remove_song(self, song_name):
-        for song in self.songs:
-            if song["name"] == song_name:
-                self.songs.remove(song)
-                print(f"{song_name} has been removed from the playlist.")
-                return
-        print(f"{song_name} not found in the playlist.")
-    
-    def display_info(self):
-        for song in self.songs:
-            print(f"{song['name']} by {song['artist']}")
+class Song:
+    def __init__(self, title, artist, year):
+        self.title = title
+        self.artist = artist
+        self.year = year
 
-def main():
-    playlist = Playlist()
-    
-    while True:
-        print("====ALBUM PLAYLIST====")
-        print("1. Add a song to the playlist")
-        print("2. Remove a song from the playlist")
-        print("3. Display the playlist")
-        print("4. Exit")
-        print("======================")
-        
-        choice = int(input("Enter your choice: "))
-        
-        if choice == 1:
-            song_name = input("Enter the name of the song: ")
-            song_artist = input("Enter the name of the artist: ")
-            playlist.add_song(song_name, song_artist)
-        
-        elif choice == 2:
-            song_name = input("Enter the name of the song: ")
-            playlist.remove_song(song_name)
-        
-        elif choice == 3:
-            playlist.display_info()
-            
-        elif choice == 4:
-            print("Exiting...")
-            break
-        
-        else:
-            print("Invalid choice. Please enter a valid option.")
-            
-main()
+class Album:
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
+        self.songs = []
+
+    def add_song(self, title, year):
+        song = Song(title, self.artist, year)
+        self.songs.append(song)
+
+class Artist:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+        self.songs = []
+
+    def add_album(self, album):
+        self.albums.append(album)
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+class Playlist:
+    def __init__(self, name):
+        self.name = name
+        self.songs = []
+
+    def add_song(self, song):
+        self.songs.append(song)
+
+# Create an artist
+artist_name = input("Enter artist's name: ")
+artist = Artist(artist_name)
+
+# Create an album
+album_title = input("Enter album title: ")
+album = Album(album_title, artist)
+
+
+num_songs = int(input("How many songs do you want to add to the album? "))
+for i in range(num_songs):
+    song_title = input(f"Enter title for song {i+1}: ")
+    release_year = int(input(f"Enter release year for song {i+1}: "))
+    album.add_song(song_title, release_year)
+
+
+song_title = input("Enter song title: ")
+release_year = int(input("Enter release year: "))
+artist_song = Song(song_title, artist, release_year)
+artist.add_song(artist_song)
+
+
+playlist_name = input("Enter playlist name: ")
+playlist = Playlist(playlist_name)
+playlist.songs.extend(album.songs)
+
+
+print("\nSongs in the playlist:")
+for song in playlist.songs:
+    print(f"{song.title} by {song.artist.name} ({song.year})")
+
+
+print("\nArtist's songs:")
+for song in artist.songs:
+    print(f"{song.title} ({song.year})")
--- Others/ZEED/PyPyFT/Playlist.py
+++ Others/ZEED/PyPyFT/Playlist.py
@@ -1,42 +1,72 @@
-class Playlist:
-    def __init__(self):
+class Song:
+    def __init__(self, title, artist, year):
+        self.title = title
+        self.artist = artist
+        self.year = year
+
+class Album:
+    def __init__(self, title, artist):
+        self.title = title
+        self.artist = artist
         self.songs = []
 
-    def addsong(self, song):
+    def add_song(self, title, year):
+        song = Song(title, self.artist, year)
         self.songs.append(song)
-        print(f"{song} has been added to the playlist.")
 
-    def displayplaylist(self):
-        if self.songs:
-            print("Your Playlist:")
-            for i, song in enumerate(self.songs, start=1):
-                print(f"{i}. {song}")
-        else:
-            print("Your playlist is empty.")
+class Artist:
+    def __init__(self, name):
+        self.name = name
+        self.albums = []
+        self.songs = []
 
-def main():
-    playlist = Playlist()
-    while True:
-        print("\nWhat would you like to do?")
-        print("1. Add a song to the playlist")
-        print("2. Display the playlist")
-        print("3. Exit")
+    def add_album(self, album):
+        self.albums.append(album)
 
-        choice = input("Enter your choice: ")
+    def add_song(self, song):
+        self.songs.append(song)
 
-        if choice == "1":
-            num_songs = int(input("How many songs would you like to add? "))
-            for _ in range(num_songs):
-                song = input("Enter the name of the song: ")
-                playlist.addsong(song)
-                
-        elif choice == "2":
-            playlist.displayplaylist()
-            
-        elif choice == "3":
-            print("Exiting...")
-            break
-        else:
-            print("Invalid choice. Please enter a valid option.")
+class Playlist:
+    def __init__(self, name):
+        self.name = name
+        self.songs = []
 
-main()
+    def add_song(self, song):
+        self.songs.append(song)
+
+# Create an artist
+artist_name = input("Enter artist's name: ")
+artist = Artist(artist_name)
+
+
+album_title = input("Enter album title: ")
+album = Album(album_title, artist)
+
+
+num_songs = int(input("How many songs do you want to add to the album? "))
+for i in range(num_songs):
+    song_title = input(f"Enter title for song {i+1}: ")
+    release_year = int(input(f"Enter release year for song {i+1}: "))
+    album.add_song(song_title, release_year)
+
+
+song_title = input("Enter song title: ")
+release_year = int(input("Enter release year: "))
+artist_song = Song(song_title, artist, release_year)
+artist.add_song(artist_song)
+
+
+playlist_name = input("Enter playlist name: ")
+playlist = Playlist(playlist_name)
+playlist.songs.extend(album.songs)
+playlist.songs.append(artist_song)  
+
+
+print("\nSongs in the playlist:")
+for song in playlist.songs:
+    print(f"{song.title} by {song.artist.name} ({song.year})")
+
+
+print("\nArtist's songs:")
+for song in artist.songs:
+    print(f"{song.title} ({song.year})")
